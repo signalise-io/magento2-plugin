b83b7d63ee423956d6387ba3ce99961b06a08e2c
Ndottens <nick@elgentos.nl>
2022-08-18 11:35:06 +0200

    Rework SignalsieApiClient

--- Model/SignaliseApiClient.php
+++ Model/SignaliseApiClient.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Signalise\Plugin\Model;
 
-use Exception;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\HTTP\AsyncClient\GuzzleAsyncClient;
 use Magento\Framework\HTTP\AsyncClient\Request;
+use Psr\Log\LoggerInterface;
+use Throwable;
 
 class SignaliseApiClient
 {
@@ -15,10 +16,16 @@ class SignaliseApiClient
 
     private SignaliseConfig $signaliseConfig;
 
-    public function __construct(GuzzleAsyncClient $client, SignaliseConfig $signaliseConfig)
-    {
+    private LoggerInterface $logger;
+
+    public function __construct(
+        GuzzleAsyncClient $client,
+        SignaliseConfig $signaliseConfig,
+        LoggerInterface $logger
+    ) {
         $this->client = $client;
         $this->signaliseConfig = $signaliseConfig;
+        $this->logger = $logger;
     }
 
     /**
@@ -37,24 +44,31 @@ class SignaliseApiClient
         return new Request(
             $this->apiUrl(),
             Request::METHOD_POST, [
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
-        ],
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json'
+            ],
             $serializedData
         );
     }
 
-    /**
-     * @throws Exception
-     */
+
     public function pushData(string $serializedData): void
     {
         try {
-            $this->client->request(
+            $response = $this->client->request(
                 $this->createRequest($serializedData)
+            )->get();
+
+
+            $this->logger->info(
+                sprintf(
+                    '%s - Pushed date to Signalise with status code: %s',
+                    date('d/m/Y'),
+                    $response->getStatusCode()
+                )
             );
-        } catch (Exception $exception) {
-            Throw new Exception($exception->getMessage());
+        } catch (Throwable $throwable) {
+            $this->logger->error($throwable->getMessage());
         }
     }
 }
