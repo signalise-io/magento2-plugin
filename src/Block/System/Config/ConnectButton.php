020f2d37d8f35abf5b15b4d1ee139fb337b86b25
Ndottens <nick@elgentos.nl>
2022-10-13 16:24:33 +0200

    added connectButton to adminthml config

---
+++ src/Block/System/Config/ConnectButton.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Copyright Elgentos BV. All rights reserved.
+ * https://www.elgentos.nl/
+ */
+
+declare(strict_types=1);
+
+namespace Signalise\Plugin\Block\System\Config;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Exception\LocalizedException;
+
+class ConnectButton extends Field
+{
+    protected $_template = 'Signalise_Plugin::system/config/button.phtml';
+
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    public function render(AbstractElement $element): string
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+
+        return parent::render($element);
+    }
+
+    protected function _getElementHtml(AbstractElement $element): string
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * @throws LocalizedException
+     */
+    public function getButtonHtml(): string
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'connect_button',
+                'label' => __('Custom Button'),
+            ]
+        );
+
+        return $button->toHtml();
+    }
+}
